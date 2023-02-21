014d03905c71e94e341b84d13b698c27d19c12cf
dwnGnL <992938550845@yandex.ru>
2023-02-21 09:28:46 +0500

    change cors

--- internal/application/cors.go
+++ internal/application/cors.go
@@ -10,7 +10,7 @@ func WithCORS() gin.HandlerFunc {
 		rw.Header().Add("Access-Control-Allow-Origin", "*")
 		rw.Header().Add("Access-Control-Expose-Headers", "Content-Length,Content-Range,X-test")
 		rw.Header().Add("Access-Control-Allow-Headers", "Referer,X-JWT,Accept,Keep-Alive,DNT,Origin,User-Agent,X-Requested-With,"+
-			"If-Modified-Since,InMemCache-Control,Content-Type,Range,authorization,Cookie,Upgrade,Connection,Sec-WebSocket-Key,"+
+			"If-Modified-Since,InMemCache-Control,Content-Type,Range,Authorization,Cookie,Upgrade,Connection,Sec-WebSocket-Key,"+
 			"Sec-WebSocket-Version,Sec-WebSocket-Extensions")
 		// rw.Header().Add("Access-Control-Allow-Credentials", "true")
 		rw.Header().Add("Access-Control-Allow-Methods", "*")
